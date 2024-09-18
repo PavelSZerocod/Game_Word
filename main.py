65864d7d6441417227d12bbb6e94a383d94d57db
PavelSZerocod <samusenko.pasha@gmail.com>
2024-09-18 13:14:38 +0300

    add new code-translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from googletrans import Translator
 
 def get_english_words():
     url = "https://randomword.com/"
@@ -17,6 +18,14 @@ def get_english_words():
     except:
         print("Произошла ошибка!")
 
+def translate_to_russian(text):
+    translator = Translator()
+    try:
+        translation = translator.translate(text, dest='ru')
+        return translation.text
+    except Exception as e:
+        print(f"Ошибка перевода: {e}")
+        return text
 
 def word_game():
     print("Добро пожаловать в игру!")
@@ -25,12 +34,15 @@ def word_game():
         word = word_dict.get("english_words")
         word_definition = word_dict.get("word_definition")
 
-        print(f"Значение слова - {word_definition}")
+        translated_word = translate_to_russian(word)
+        translated_definition = translate_to_russian(word_definition)
+
+        print(f"Значение слова - {translated_definition}")
         user = input("Что это за слово? ")
-        if user == word:
+        if user.lower() == translated_word.lower():
             print("Ответ верный!")
         else:
-            print(f"Ответ не верный, верный ответ - {word}")
+            print(f"Ответ не верный, верный ответ - {translated_word}")
 
         play_again = input("Хотите сыграть еще раз? y/n")
         if play_again != "y":
